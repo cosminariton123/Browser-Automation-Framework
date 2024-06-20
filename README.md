26982d8c537edaf3ae5d4aa49f9ee6f89d3e0a4b
cosminariton123 <cosmin.ariton@gmail.com>
2024-06-20 22:51:56 +0300

    Updated README and added an example in main.py

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Browser-Automation-Framework
+# Browser-Automation-Framework using Selenium with Python
+
+It is designed to work with any website by using Selenium(an E2E testing framework).\
+It allows for easier and faster implementations at the cost of running speed.\
+
+![Showcase](./BrowserAutomationFramework%20preview.gif)
+
+# Usage
+
+Provide the driver executable in the driver_executables directory. \
+Provide an implementation in main.py\
+
+# Configuration
+
+## Default driver options
+
+Add selenium options for the desired driver like --start-maximized, --incognito, --headless .
+
+## Paths
+
+Update paths.py to reflect the names of the provided files/drivers as per example\
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import core.drivers.driver_instance as driver_instance
-
+from core.element_type.input_textbox import InputTextBox
+from core.element_type.textbox import TextBox
 
 def main():
     driver_instance.DRIVER = driver_instance.get_new_default_chrome_driver()
 
-    #ENTER CODE HERE
+    #EXAMPLE CODE(IF NEEDED, SURROUND WITH TRY CATCH)
+    import time
+    item = "Distance to the Mars"
+    timeout_amount = 10
+    URL = "https://www.bing.com/"
+
+    driver_instance.DRIVER.get(URL)
+    time.sleep(2)   #Added delay to help visualize the framework in action
+
+    search_box = InputTextBox(xpath="//input[@id=\"sb_form_q\"]", timeout=timeout_amount)
+    search_box.search(item)
+    time.sleep(2)   #Added delay to help visualize the framework in action
+
+    result_box = TextBox(xpath="//div[@id=\"d_ans\"]/div/div[1]/div/div[1]", timeout=timeout_amount)
+    result = result_box.get_text()
+
+    driver_instance.DRIVER.execute_script("document.evaluate('//div[@id=\"d_ans\"]/div/div[1]/div/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.borderColor = 'red';")
+    driver_instance.DRIVER.execute_script("document.evaluate('//div[@id=\"d_ans\"]/div/div[1]/div/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.borderStyle = 'solid';")
+    driver_instance.DRIVER.execute_script("document.evaluate('//div[@id=\"d_ans\"]/div/div[1]/div/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.borderWidth = '30px';")
+    
+    time.sleep(3) #Added delay to help visualize the framework in action
+    print(result)
 
     driver_instance.DRIVER.quit()
     driver_instance.DRIVER = None
